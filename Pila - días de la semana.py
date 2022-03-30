607dcbefa413ac3223ba28a91572b6659cf091dd
CamilaParedesRosso <99370302+CamilaParedesRosso@users.noreply.github.com>
2022-03-30 19:27:18 -0400

    Add files via upload

---
+++ Pila - días de la semana.py
@@ -0,0 +1,24 @@
+#Ejemplo de Pila - LIFO
+pila=[]
+#Incluir elementos - apilar
+pila.append('Lunes')
+pila.append('Martes')
+pila.append('Miercoles')
+pila.append('Jueves')
+pila.append('Viernes')
+pila.append('Sabado')
+pila.append('Domingo')
+print(pila)
+#Sacar elementos - pop- desapilar
+x=pila.pop()
+print(pila)#lo restante
+x=pila.pop()
+print(pila)
+x=pila.pop()
+print(pila)
+x=pila.pop()
+print(pila)
+x=pila.pop()
+print(pila)
+x=pila.pop()
+print(pila)
